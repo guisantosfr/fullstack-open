81978904bc671b21e2a1cfe82c56856e6e2b3785
Guilherme Santos <santosgui678@gmail.com>
2023-03-07 21:52:52 -0300

    Finish exercise 1.2

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -1,8 +1,12 @@
+import Part from './Part';
+
 const Content = (props) => {
   return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
+    <div>
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
+    </div>
   )
 }
 
