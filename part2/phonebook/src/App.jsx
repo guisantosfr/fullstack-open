4e9e96ee0bf293fb8ee9bf8e30cfa205fb440136
Guilherme Santos <santosgui678@gmail.com>
2023-03-11 11:35:43 -0300

    Exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,11 +2,16 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
   const addContact = (e) => {
     let alreadyExists = false
@@ -19,7 +24,7 @@ const App = () => {
 
     alreadyExists ?
       alert(`${newName} is already added to phonebook`) :
-      setPersons(persons.concat({ name: newName, number: newNumber }))
+      setPersons(persons.concat({ name: newName, number: newNumber, id: persons.length + 1 }))
 
     setNewName('')
     setNewNumber('')
@@ -33,9 +38,25 @@ const App = () => {
     setNewNumber(e.target.value)
   }
 
+  const handleSearchChange = (e) => {
+    if (e.target.value !== '')
+      setShowAll(false);
+
+    setSearch(e.target.value);
+  }
+
+  const contactsToShow = showAll
+    ? persons
+    : persons.filter(person => person.name.toUpperCase().includes(search.toUpperCase()));
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={search} onChange={handleSearchChange} />
+      </div>
+
+      <h2>Add a new</h2>
       <form onSubmit={addContact}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -47,10 +68,11 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
       <ul>
         {
-          persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)
+          contactsToShow.map(person => <li key={person.name}>{person.name} {person.number}</li>)
         }
       </ul>
     </div>
