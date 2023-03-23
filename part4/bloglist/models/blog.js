28e41e99bd4bca9ac838cbfcd6eb0e1ebec99ef0
Guilherme Santos <santosgui678@gmail.com>
2023-03-23 10:46:29 -0300

    Refactoring code

--- part4/bloglist/models/blog.js
+++ part4/bloglist/models/blog.js
@@ -1,13 +1,5 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-const mongoUrl = process.env.MONGODB_URL
-
-mongoose.connect(mongoUrl)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(error => console.log(`Error connecting to ${error.message}`))
-
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
