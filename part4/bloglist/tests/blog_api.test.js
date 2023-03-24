6809a24bee838cb1429abed9c47b014b58e1155a
Guilherme Santos <santosgui678@gmail.com>
2023-03-24 10:54:31 -0300

    Exercises 4.8 to 4.10

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+
+  await Promise.all(promiseArray)
+})
+
+test('blogs are returned as json', async () => {
+  await api.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/)
+}, 100000)
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+test("blog has id property", async () => {
+  await api.get("/api/blogs").then((response) => {
+    expect(response.body[0].id).toBeDefined();
+  });
+});
+
+test('a specific blog is within the returned blogs', async () => {
+  const response = await api.get('/api/blogs')
+
+  const titles = response.body.map(r => r.title)
+  expect(titles).toContain(
+    'Go To Statement Considered Harmful'
+  )
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: "Async/Await",
+    author: "Gui",
+    url: "fullstack.com",
+    likes: 3
+  }
+
+  await api.post('/api/blogs').send(newBlog).expect(201).expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+  const titles = blogsAtEnd.map(r => r.title)
+  expect(titles).toContain('Async/Await')
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
