d0c3f4312a8ce3633eff5696459b7b0376758ada
Guilherme Santos <santosgui678@gmail.com>
2023-03-27 10:09:53 -0300

    Exercise 4.15

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,6 +4,7 @@ require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
@@ -21,5 +22,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use(blogsRouter)
+app.use(usersRouter)
 
 module.exports = app
