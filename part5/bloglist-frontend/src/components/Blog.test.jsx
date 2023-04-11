66b2d4570d45e47542f2de1f6ba83901498d480c
Guilherme Santos <santosgui678@gmail.com>
2023-04-11 10:59:22 -0300

    Exercise 5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,8 +1,11 @@
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('Blog Tests', () => {
+  let container
+
   const blog = {
     title: 'Fullstack Open',
     author: 'University of Helsinki',
@@ -13,13 +16,25 @@ describe('Blog Tests', () => {
     }
   }
 
-  test('renders only main content', () => {
-    const { container } = render(<Blog blog={blog} />)
+  beforeEach(() => {
+    container = render(<Blog blog={blog} />).container
+  })
 
+
+  test('renders only main content', () => {
     const div = container.querySelector('.mainInfo')
     expect(div).toHaveTextContent('Fullstack Open by University of Helsinki')
 
     const div2 = container.querySelector('.subInfo')
     expect(div2).toHaveStyle('display: none')
+  })
+
+  test('renders sub content if button is clicked', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const div = container.querySelector('.subInfo')
+    expect(div).not.toHaveStyle('display: none')
   })
 })
